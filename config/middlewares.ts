509eaf9c560550176fcaa35d03c1a8a478703af0
nsandoya <natsandoya@gmail.com>
2024-12-24 17:04:33 -0500

    fix: Update middleware configs for Cloudinary

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -8,7 +8,7 @@ export default [
         useDefaults: true,
         directives: {
           'connect-src': ["'self'", 'https:'],
-          'img-src': ["'self'", 'data:', 'blob:', 'res.cloudinary.com'],
+          'img-src': ["'self'", 'data:', 'blob:', 'https://res.cloudinary.com'],
           'media-src': ["'self'", 'data:', 'blob:', 'res.cloudinary.com'],
           upgradeInsecureRequests: null,
         },
